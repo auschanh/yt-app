3495fde35cd220095cd65e25c3ed9fb0c92e5575
Austin Chanhsavang <auschanh@MacBook-Pro.cgocable.net>
2020-08-16 06:20:32 -0400

    ready for deploy

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import youtube from '../apis/youtube';
+
+const KEY = 'AIzaSyDcYC6MX-LWe9FaF0I2L2bEUFBixNfQUEM';
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+        q: term,
+      },
+    });
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+};
+
+export default useVideos;
